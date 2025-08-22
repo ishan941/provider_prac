9ccbb57e5c5a845827d35caf05638747b0d12065
Ishan Shrestha <iahan.esan18@gmail.com>
2025-08-22 14:25:12 +0545

    Refactor CustomTextFormField to use final fields for better immutability and maintainability

--- lib/widgets/custom_text_form_field.dart
+++ lib/widgets/custom_text_form_field.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  String? hintText;
-  bool obscureText;
-  Widget? prefixIcon;
-  Widget? suffixIcon;
-  TextEditingController? controller;
-  CustomTextFormField({
+  final String? hintText;
+  final bool obscureText;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final TextEditingController? controller;
+  const CustomTextFormField({
     this.hintText,
     this.obscureText = false,
     this.prefixIcon,
@@ -17,18 +17,21 @@ class CustomTextFormField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide(color: Colors.grey),
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          hintText: hintText,
+          prefixIcon: prefixIcon,
+          suffixIcon: suffixIcon,
         ),
-        hintText: hintText,
-        prefixIcon: prefixIcon,
-        suffixIcon: suffixIcon,
+        obscureText: obscureText,
+        controller: controller,
       ),
-      obscureText: obscureText,
-      controller: controller,
     );
   }
 }
